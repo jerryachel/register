234a8d0123b60b5d4cd87913fdf50832365e8b7c
jerryachel <jerryachel@foxmail.com>
2017-12-18 23:15:06 +0800

    添加部分接口

--- src/views/register_form/register_form.vue
+++ src/views/register_form/register_form.vue
@@ -155,6 +155,7 @@ export default {
   		}
   	},
   	enoughPopup(){
+  		let wxConfig
   		axios.get('clinic/numberLeft.do',{
   			params:{
 				dateTime:this.$route.query.date,
@@ -170,7 +171,7 @@ export default {
   						total_fee:4.5
   					}
   				}).then(({data})=>{
-  					console.log(data)
+  					wxConfig = data.model
   				})
   			}
   		})
@@ -179,15 +180,17 @@ export default {
 		function onBridgeReady(){
 		   WeixinJSBridge.invoke(
 		       'getBrandWCPayRequest', {
-		           "appId":"wx2421b1c4370ec43b",     //公众号名称，由商户传入     
-		           "timeStamp":"1395712654",         //时间戳，自1970年以来的秒数     
-		           "nonceStr":"e61463f8efa94090b1f366cccfbbb444", //随机串     
-		           "package":"prepay_id=u802345jgfjsdfgsdg888",     
-		           "signType":"MD5",         //微信签名方式：     
-		           "paySign":"70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 
+		           "appId":wxConfig.appId,     //公众号名称，由商户传入     
+		           "timeStamp":wxConfig.timeStamp,         //时间戳，自1970年以来的秒数     
+		           "nonceStr":wxConfig.nonceStr, //随机串     
+		           "package":wxConfig.prepayId,     
+		           "signType":wxConfig.signType,         //微信签名方式：     
+		           "paySign":wxConfig.paySign //微信签名 
 		       },
 		       function(res){     
-		           if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+		           if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+
+		           }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
 		       }
 		   ); 
 		}
@@ -208,6 +211,9 @@ export default {
 		  message: `当前剩余名额不足(余${num}人)<br>无法进行预约`,
 		  //showCancelButton: true
 		});
+  	},
+  	confirmOrder(orderId){
+  		axios.post('clinic/order.do',{})
   	}
 
 
