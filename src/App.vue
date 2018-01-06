7a0eca28fdc1c0bde26a3fdebd1371e25b362a8d
jerryachel <jerryachel@foxmail.com>
2018-01-06 19:15:52 +0800

    接入微信分享

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,71 @@
 
 <script>
 import axios from './service/axios.js'
+import wx from 'weixin-js-sdk'
 export default {
 	name: 'app',
 	created(){
 		let openId = this.$route.query.openId || sessionStorage.getItem('uid')
 		sessionStorage.setItem('uid', openId);
-		console.log(openId)
+		//登录
 		axios.get('clinic/login.do',{
 			params:{
 				openId:openId
 			}
+		}).then(()=>{
+			//console.log(window.location.href.replace("#/", ""))
+			//配置wx.config
+		    axios.get('clinic/jsApiConfig.do',{
+		        params: {
+		            currentUrl: window.location.href
+		        }
+		    }).then(({data})=>{
+		        console.log(data)
+		        let res = data.model
+		        sessionStorage.setItem('shareUrl', res.shareUrl);
+		        wx.config({
+		            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		            appId: res.appId, // 必填，公众号的唯一标识
+		            timestamp: res.timestamp, // 必填，生成签名的时间戳
+		            nonceStr: res.nonceStr, // 必填，生成签名的随机串
+		            signature: res.signature, // 必填，签名，见附录1
+		            jsApiList: [
+			            'onMenuShareAppMessage',
+			            'onMenuShareTimeline'
+		            ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		        })
+		        wx.ready(function() {
+					wx.onMenuShareAppMessage({
+						title: '陈景辉诊所', // 分享标题
+						desc: '欢迎来到陈景辉诊所！', // 分享描述
+						link: 'http://www.gzbige.com/dist/#/?openId=', // 分享链接
+						imgUrl: '', // 分享图标
+						type: '', // 分享类型,music、video或link，不填默认为link
+						dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+						success: function() {
+						// 用户确认分享后执行的回调函数
+						},
+						cancel: function() {
+						// 用户取消分享后执行的回调函数
+						}
+					});
+					wx.onMenuShareTimeline({
+						title: '陈景辉诊所', // 分享标题
+						link: 'http://www.gzbige.com/dist/#/?openId=',
+						imgUrl: '', // 分享图标
+						success: function() {
+						// 用户确认分享后执行的回调函数
+						},
+						cancel: function() {
+						// 用户取消分享后执行的回调函数
+						}
+					});
+				});
+		    })
 		})
+	},
+	mounted(){
+		
 	}
 
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
+//openId为空则跳转菜单
+if (!window.location.hash.split('openId=')[1]) {
+   window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxaf63b620a0b0110d&redirect_uri=http%3A%2F%2Fregister.gzbige.com%2Fclinic%2Fredirect.do&response_type=code&scope=snsapi_userinfo&state=HOME#wechat_redirect"
+}
 // 格式化时间
-Date.prototype.Format = function (fmt) { //author: 
-    var o = {
-        "M+": this.getMonth() + 1, //月份
-        "d+": this.getDate(), //日
-        "h+": this.getHours(), //小时
-        "m+": this.getMinutes(), //分
-        "s+": this.getSeconds(), //秒
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-        "S": this.getMilliseconds() //毫秒
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
+Date.prototype.Format = function(fmt) { //author: 
+  var o = {
+    "M+": this.getMonth() + 1, //月份
+    "d+": this.getDate(), //日
+    "h+": this.getHours(), //小时
+    "m+": this.getMinutes(), //分
+    "s+": this.getSeconds(), //秒
+    "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+    "S": this.getMilliseconds() //毫秒
+  };
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
     if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
+  return fmt;
 }
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
@@ -20,16 +24,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/css/public.css'
+import axios from './service/axios.js'
 import './assets/js/flexible.min.js'
-import {InfiniteScroll} from 'mint-ui'
-Vue.use(InfiniteScroll);
+import wx from 'weixin-js-sdk'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
 })
